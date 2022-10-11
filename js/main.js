b4c29125342c704ea87e31964ebe8d582b53a68a
Лилиана Леднёва <lilyledneva@gmail.com>
2022-10-11 13:21:42 +0300

    Добавляет вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function randomNumber(min, max) {
+  if (isNaN(min) || isNaN(max)) {
+    return NaN
+  }
+  if (Number.isInteger(max / min)) {
+  if (min >=0 && max >= min) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+  if (max >=0 && max <= min) {
+    return Math.floor(Math.random() * (min - max + 1) ) + max;
+    }
+  }
+  console.log("Введите корректное число")
+}
+
+randomNumber();
+
+function longString (string, lenght) {
+  return string.lenght <= lenght;
+}
+
+longString("", 140)
